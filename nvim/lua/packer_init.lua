6e3ed237418b089959dca3a5f82e35f532e7fa6b
Urs Mayr <mayr.urs@gmail.com>
2022-10-31 14:08:44 +0100

    Add autocompletion

--- nvim/lua/packer_init.lua
+++ nvim/lua/packer_init.lua
@@ -9,6 +9,18 @@ return require('packer').startup(function(use)
   -- Configurations for Nvim LSP
   use 'neovim/nvim-lspconfig'
 
+  -- autocomplete
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = {
+      'L3MON4D3/LuaSnip',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
+      'saadparwaiz1/cmp_luasnip',
+    },
+  }
+
   -- colorschemes
   use 'tanvirtin/monokai.nvim'
   use { 'rose-pine/neovim', as = 'rose-pine', commit = '69dca24ba7f8e74f1e6f0bacbc93481ac4047f2e'} 
