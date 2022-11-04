669d44fae68dd2538a36d2e40b489afde7acba5d
Urs Mayr <mayr.urs@gmail.com>
2022-11-04 08:02:45 +0100

    Add Terminal mapping

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,6 +1,9 @@
-
 local map = vim.api.nvim_set_keymap
+local default_opts = { noremap = true, silent = true }
 local cmd = vim.cmd
 
 -- map Esc to jk
 map('i', 'jk', '<Esc>', {noremap = true})
+
+-- open terminal
+map('n', '<C-t>', ':10sp term://zsh<CR>', { noremap = true })
